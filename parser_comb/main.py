645f36de8cb2769400f8fcd41382cb6b4d04a042
kibrq <ki.br178@gmail.com>
2020-10-15 01:55:45 +0300

    Add keys for cli

--- parser_comb/main.py
+++ parser_comb/main.py
@@ -4,7 +4,7 @@ import argparse as ap
 
 import sys
 
-from parser import parse
+from parser import *
 
 from prolog_ast import ASTPrinter
 
@@ -13,6 +13,14 @@ def init_argparse():
     parser = ap.ArgumentParser()
     parser.add_argument('--pretty', dest='pretty',
                         action='store_true', help='prints pretty')
+    parser.add_argument('--atom', dest='atom', action='store_true')
+    parser.add_argument('--typeexpr', dest='typeexpr', action='store_true')
+    parser.add_argument('--type', dest='type', action='store_true')
+    parser.add_argument('--module', dest='module', action='store_true')
+    parser.add_argument('--relation', dest='relation', action='store_true')
+    parser.add_argument('--list', dest='list', action='store_true')
+    parser.add_argument('--prog', dest='prog', action='store_true')
+
     parser.add_argument('inputfile', type=str, help='source file')
     parser.add_argument('outputfile', type=str, nargs='?', help='output file')
     return parser
@@ -24,14 +32,25 @@ def main(argv):
         lines = []
         with open(args.inputfile, 'r') as file:
             lines = file.read()
-        res = parse(lines)
-
-        if res is None:
-            print('Syntax error')
-            return
+        func = None
+        if args.atom:
+            func = atom
+        elif args.typeexpr:
+            func = subtype
+        elif args.type:
+            func = def_type
+        elif args.module:
+            func = def_module
+        elif args.relation:
+            func = def_rel
+        elif args.list:
+            func = array
+        else:
+            func = prog
+        res = (func << eof()).parse(lines)
 
         if args.outputfile is None:
-              args.outputfile = args.inputfile + '.out'
+            args.outputfile = args.inputfile + '.out'
 
         file = open(args.outputfile, 'w')
 
@@ -41,6 +60,9 @@ def main(argv):
 
         if not file is None:
             file.close()
+
+    except ParseError:
+        print('Syntax Error')
 
     except FileNotFoundError:
         print('There is no such file to open')
